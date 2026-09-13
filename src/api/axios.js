981c97b6db2b77919d321f352038bb1ba06fb4e5
Nadeera Kumara <nadeerakumara140@gmail.com>
2026-09-13 20:52:34 +0530

    Configure production backend API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,22 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-})
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-})
+  const token = localStorage.getItem('token');
 
-export default api
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
